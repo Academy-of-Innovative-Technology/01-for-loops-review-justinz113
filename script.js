3592ffb66760b2e95c14b128c6ad0f489d2e4ba9
justinz113 <justinz113@nycstudents.net>
2025-11-13 13:59:09 -0500

    began to make changes

---
+++ script.js
@@ -0,0 +1,23 @@
+// CREATE EVENT LISTENERS
+
+// CREATE FUNCTIONS W/ LOOPS
+
+function countup15(num) {
+  for (let a = num; a <= 15; a++) {
+    console.log(a);
+  }
+}
+
+function countdown15(num) {}
+
+function countupOdd15(num) {}
+
+function multiple5(num) {}
+
+function fullRange50(num) {}
+
+function multiple2(num) {}
+
+// playgrioudn
+
+document.querySelector(".countup").addEventListener("click", countup15(0));
